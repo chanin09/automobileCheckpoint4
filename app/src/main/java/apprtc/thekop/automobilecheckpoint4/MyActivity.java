210abf1ff20eae3de77a8779f08220128b9d91ff
chanin <chaninklibpeng@gmail.com>
2015-12-20 22:15:17 +0700

    Start Project

---
+++ app/src/main/java/apprtc/thekop/automobilecheckpoint4/MyActivity.java
@@ -0,0 +1,13 @@
+package apprtc.thekop.automobilecheckpoint4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_layout);
+    }
+}
