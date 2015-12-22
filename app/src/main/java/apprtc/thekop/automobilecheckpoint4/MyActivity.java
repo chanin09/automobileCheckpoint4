b58ebb85e44a6fbf25f45cb52cb5ef91fbc2cd7a
chanin <chaninklibpeng@gmail.com>
2015-12-22 18:43:36 +0700

    อีกสักรอบ

--- app/src/main/java/apprtc/thekop/automobilecheckpoint4/MyActivity.java
+++ app/src/main/java/apprtc/thekop/automobilecheckpoint4/MyActivity.java
@@ -8,6 +8,6 @@ public class MyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.map_layout);
+        setContentView(R.layout.activity_my);
     }
 }
